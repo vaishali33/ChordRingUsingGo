f9e58c583ea7ad0d630e849bede712a2074275f2
TilakNeha <tilak.neha@gmail.com>
2015-04-25 20:06:28 -0400

    Added full functionality for createRing, FindSuccessor
    Following things work at this stage-
    1. ChordRing is formed
    2. Lookup sends queries along the ring
    3. Notify RPC for updating predecessor
    4. Id field removed from requests
    5. Permission string added
    6. Added belongsto method
    
    Pending tasks-
    1. Need to merge changes for Delete, Insert , Update methods etc
    2. Need to merge changes for stabilize routines
    3. Add fix-fingers method
    4. Add node-leaving logic

--- JsonDict3Server.go
+++ JsonDict3Server.go
@@ -12,6 +12,9 @@ import (
     "strings"
     "io/ioutil"
     "crypto/sha1"
+    "math"
+    "time"
+    "reflect"
 )
 
 type DKey struct{
@@ -20,7 +23,7 @@ type DKey struct{
 type Request struct{
 	KeyRel DKey
 	Val map[string]interface{}
-	Id int
+        Permission string
 }
 type Response struct{
 	Tripair Triplet
@@ -40,7 +43,12 @@ type ListResponse struct{
 	Err error
 }
 
-type DIC3 int
+type NodeInfo struct {
+        Chordid int
+        Address string
+}
+
+type DIC3 struct{}
 
 var dict3 map[DKey]map[string]interface{}
 var configMap map[string]interface{}
@@ -50,27 +58,32 @@ var dict3File string
 var methods []interface{}
 var port string
 var confLoc string
-var successor string
+var successor NodeInfo
+var predecessor  NodeInfo
 var shakey string
 var chordid int
-var fingertable map[int]string
+var selfaddr string
+var fingertable map[int]NodeInfo
+var purgeChan chan struct{}
+var purgeInterval int
 
 //server shutdown
 func (t *DIC3) Shutdown(dKey *DKey, reply *int) error{
 	fmt.Println("shuting server down!!!!!!")
 	*reply = 9
 	persistDict3()
+        close(purgeChan)
 	os.Exit(0)
 	return nil
 }
 //Lookup returns value stored for given key and relation
 func (t *DIC3) Lookup(req *Request, reply *Response) error {
-	fmt.Println("in Lookup : ", req.KeyRel.KeyA, req.KeyRel.RelA, req.Id)
+	fmt.Println("in Lookup : ", req.KeyRel.KeyA, req.KeyRel.RelA)
 	val := dict3[req.KeyRel]
 
         if val != nil {
 
-                reply.ID = req.Id
+                val["accessed"] = time.Now()
            	reply.Tripair.Key = req.KeyRel.KeyA
            	reply.Tripair.Rel = req.KeyRel.RelA
            	reply.Tripair.Val = val
@@ -80,6 +93,9 @@ func (t *DIC3) Lookup(req *Request, reply *Response) error {
         } else {
 
            krhash  := getKeyRelHash(req.KeyRel.KeyA,req.KeyRel.RelA)
+           if krhash == chordid {
+              return nil
+           }
            service := findSuccessor(krhash)
            client, err := jsonrpc.Dial(protocol, service)
            if err != nil {
@@ -105,21 +121,85 @@ func (t *DIC3) Lookup(req *Request, reply *Response) error {
         return nil
 
 }
+
+//Partial Key Lookup
+func (t *DIC3) Partial_Key_Lookup(req *Request, reply *ListResponse) error {
+        fmt.Println("in Partial_Key_Lookup : ", req.KeyRel.KeyA)
+
+        result := make([]Triplet, 1)
+        for k, v := range dict3{
+                if(strings.EqualFold(k.KeyA, req.KeyRel.KeyA)){
+                        result = append(result, Triplet{k.KeyA, k.RelA, v})
+                        v["accessed"] = time.Now()      //should it be changed?
+                }
+        }
+        reply.List = result
+        reply.Err = nil
+        return nil
+}
+
+//Partial Relation Lookup
+func (t *DIC3) Partial_Rel_Lookup(req *Request, reply *ListResponse) error {
+        fmt.Println("in Partial_Rel_Lookup : ", req.KeyRel.RelA)
+        result := make([]Triplet, 1)
+        for k, v := range dict3{
+                if(strings.EqualFold(k.RelA, req.KeyRel.RelA)){
+                        result = append(result, Triplet{k.KeyA, k.RelA, v})
+                        v["accessed"] = time.Now()      //should it be changed?
+                }
+        }
+        reply.List = result
+        reply.Err = nil
+        return nil
+}
+
 //Insert a given triplet in DICT3
 func (t *DIC3) Insert(triplet *Request, reply *Response) error {
-	fmt.Println("in Insert : ", triplet.KeyRel.KeyA, triplet.KeyRel.RelA, triplet.Val, triplet.Id)
+	fmt.Println("in Insert : ", triplet.KeyRel.KeyA, triplet.KeyRel.RelA, triplet.Val)
 
-	dict3[DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}] = triplet.Val
-	_, ok := dict3[DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}]
-	reply.ID = triplet.Id
-	reply.Done = ok
-	reply.Err = nil
-	return nil
+        hashid := getKeyRelHash(triplet.KeyRel.KeyA, triplet.KeyRel.RelA)
+        if belongsto(hashid, predecessor.Chordid, chordid) == true {
+        	dict3[DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}] = triplet.Val
+        	_, ok := dict3[DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}]
+        	reply.Done = ok
+        	reply.Err = nil
+        	return nil
+       }
+       np := nearestPredecessor(hashid)
+       client, err := jsonrpc.Dial(protocol, np.Address)
+       if err != nil {
+            log.Fatal("dialing:", err)
+       }
+       var reply2 Response
+       RpcCall := client.Go("DIC3.Insert", triplet, &reply2,nil)
+       replyCall := <-RpcCall.Done
+       if replyCall != nil {
+       }
+
+       reply.ID = reply2.ID
+       reply.Done = reply2.Done
+       reply.Err = reply2.Err
+       fmt.Println(reply)
+       client.Close()
+       return nil
+}
+
+func insertTripletToDict3(dkey DKey, val map[string]interface{}, perm string){
+        valJson := make(map[string]interface{})
+        valJson["content"] = val
+        valJson["size"] = reflect.TypeOf(val).Size()
+        valJson["created"] = time.Now()
+        valJson["accessed"] = time.Now()
+        valJson["modified"] = time.Now()
+        valJson["permission"] = perm
+
+        dict3[DKey{dkey.KeyA, dkey.RelA}] = valJson
+
 }
 
 //InsertOrUpdate given triplet in DICT3
 func (t *DIC3) InsertOrUpdate(triplet *Request, reply *Response) error {
-	fmt.Println("in InsertOrUpdate : ", triplet.KeyRel.KeyA, triplet.KeyRel.RelA, triplet.Val, triplet.Id)
+	fmt.Println("in InsertOrUpdate : ", triplet.KeyRel.KeyA, triplet.KeyRel.RelA, triplet.Val)
 	keyRel := DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}
 	_, ok := dict3[keyRel]
 	if !ok {
@@ -132,7 +212,6 @@ func (t *DIC3) InsertOrUpdate(triplet *Request, reply *Response) error {
 		delete(dict3, keyRel)
 		dict3[DKey{triplet.KeyRel.KeyA, triplet.KeyRel.RelA}] = triplet.Val
 	}
-	reply.ID = triplet.Id
 	reply.Done = ok
 	reply.Err = nil
 	return nil
@@ -140,10 +219,9 @@ func (t *DIC3) InsertOrUpdate(triplet *Request, reply *Response) error {
 
 //Delete from DICT3
 func (t *DIC3) Delete(req *Request, reply *Response) error {
-	fmt.Println("in Delete : ", req.Id, req.KeyRel.KeyA, req.KeyRel.RelA)
+	fmt.Println("in Delete : ", req.KeyRel.KeyA, req.KeyRel.RelA)
 	delete(dict3, req.KeyRel)
 	fmt.Println("after delete DICT3 ", dict3)
-	reply.ID = req.Id
 	reply.Done = true
 	reply.Err = nil
 	return nil
@@ -151,7 +229,6 @@ func (t *DIC3) Delete(req *Request, reply *Response) error {
 
 //list keys in DICT3
 func (t *DIC3) Listkeys(req *Request, reply *ListResponse) error {
-	fmt.Println("in ListKeys:: Request ID : ", req.Id)
 	keys := make([]string, 0, len(dict3))
     for k := range dict3 {
     	found := checkIfPresent(k.KeyA, keys)
@@ -159,7 +236,6 @@ func (t *DIC3) Listkeys(req *Request, reply *ListResponse) error {
         	keys = append(keys, k.KeyA)
         	}
     }
-	reply.Id = req.Id
 	reply.List = keys
 	reply.Err = nil
 	return nil
@@ -167,28 +243,129 @@ func (t *DIC3) Listkeys(req *Request, reply *ListResponse) error {
 
 //list key-relation pairs in DICT3
 func (t *DIC3) ListIDs(req *Request, reply *ListResponse) error {
-	fmt.Println("in ListIDs:: Request ID : ", req.Id)
 	keys := make([]string, 0, len(dict3))
     for k := range dict3 {
         keys = append(keys, "[",k.KeyA,",", k.RelA, "]")
     }
-	reply.Id = req.Id
 	reply.List = keys
 	reply.Err = nil
 	return nil
 }
 
+func (t *DIC3) Notify(request NodeInfo, reply *NodeInfo) error {
+        if predecessor.Chordid == -1 ||
+belongsto(request.Chordid,predecessor.Chordid,chordid) == true {
+           predecessor.Chordid = request.Chordid
+           predecessor.Address = request.Address
+           return nil
+        }
+        return nil
+}
+
+func (t *DIC3) FindSuccessor(request NodeInfo, reply *NodeInfo) error {
+        fmt.Println("Control in FindSuccessor method")
+        fmt.Println("Requesting node", request)
+        if belongsto(request.Chordid, chordid, successor.Chordid) == true {
+                reply.Chordid = successor.Chordid
+                reply.Address = successor.Address
+                prevSuc := successor
+                successor.Chordid = request.Chordid
+                successor.Address = request.Address
+                fingertable[1] = successor
+                fmt.Println("Updated successor as ",successor)
+
+                // Notify previous successor
+                client1, err := jsonrpc.Dial(protocol, prevSuc.Address)
+                if err != nil {
+                     log.Fatal("dialing:", err)
+                }
+                var replynull NodeInfo
+
+                RpcCall := client1.Go("DIC3.FindNotify", request, &replynull,nil)
+                replyCall := <-RpcCall.Done
+                if replyCall != nil {
+                }
+                client1.Close()
+
+                return nil
+        } else {
+                np := nearestPredecessor(request.Chordid)
+                client, err := jsonrpc.Dial(protocol, np.Address)
+                if err != nil {
+                     log.Fatal("dialing:", err)
+                }
+                var reply2 NodeInfo
+                RpcCall := client.Go("DIC3.FindSuccessor", request, &reply2,nil)
+                replyCall := <-RpcCall.Done
+                if replyCall != nil {
+                }
+                client.Close()
+                    reply.Address = reply2.Address
+                    reply.Chordid = reply2.Chordid
+        }
+        return nil
+}
+
+func belongsto (num int, start int, end int) bool {
+        diff1 := end - start
+        if diff1 <= 0 {
+           diff1 = diff1 + 32
+        }
+        diff2 := num - start
+        if diff2 <= 0 {
+           diff2 = diff2 + 32
+        }
+        if diff1 > diff2 {
+           return true
+        }
+        return false
+}
+
+//background process for purging unused triplets
+func purgeUnusedTriplets(){
+	//not prefered because it just considers gap between execution, not schedule
+	//time.AfterFunc(time.Second*time.Duration(5), purgeUnusedTriplets)
+
+	purgeTicker := time.NewTicker(time.Duration(purgeInterval) * time.Second)
+	purgeChan = make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-purgeTicker.C:
+				fmt.Println("Purging Unused triplets ", time.Now())
+				for k, v := range dict3{
+					access := v["permission"].(string)
+					if(strings.EqualFold("RW", access)){
+						accessed := v["accessed"].(string)
+						access_time, err := time.Parse("2006-01-02 15:04", accessed)
+						checkError(err)
+						duration := time.Since(access_time)
+						if(duration > time.Duration(purgeInterval)){
+							//delete triplet
+							delete(dict3, k)
+						}
+					}
+				}
+			case <-purgeChan:
+				purgeTicker.Stop()
+				fmt.Println("Stopped the purgeTicker!")
+				return
+			}
+		}
+	}()
+}
+
 func main() {
 
 	loadConfig()
-        chordid = getChordId(port,ipAdd)
+        //chordid = getChordId(port,ipAdd)
 	loadDict3()
-        fingertable = make(map[int]string)
-        sid := getChordId(successor,ipAdd)
-        ls := []string{ipAdd,successor}
-        fingertable[sid] = strings.Join(ls,"")
+        fingertable = make(map[int]NodeInfo)
+        //sid := getChordId(successor,ipAdd)
+        //ls := []string{ipAdd,successor}
+        //fingertable[sid] = strings.Join(ls,"")
 
-        //makeChordRing()
+        createChordRing()
 
 	dic3 := new(DIC3)
 	rpc.Register(dic3)
@@ -199,8 +376,6 @@ func main() {
 	listener, err := net.ListenTCP(protocol, tcpAddr)
 	checkError(err)
 
-        getRelHash("Neha")
-        getKeyHash("Tilak2")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -224,8 +399,13 @@ func loadConfig() error{
 	protocol = configMap["protocol"].(string)
 	ipAdd = configMap["ipAddress"].(string)
 	port = configMap["port"].(string)
-        successor = configMap["successor"].(string)
-	persiStorage := configMap["persistentStorageContainer"].(map[string]interface{})
+        addr := []string{ipAdd,port}
+        selfaddr = strings.Join(addr,"")
+        if selfaddr == "" {
+          fmt.Println("Could not initialize selfaddr")
+        }
+        chordid = getChordId(port,ipAdd)
+         persiStorage := configMap["persistentStorageContainer"].(map[string]interface{})
 	dict3File = persiStorage["file"].(string)
 	methods = configMap["methods"].([]interface{})
 	fmt.Println("Methods exposed by server: ", methods)
@@ -351,21 +531,60 @@ func getChordId(po string, ip string) int{
            return cid
 }
 
+func createChordRing() {
+           predecessor.Chordid = -1
+           successor.Address = selfaddr
+           successor.Chordid = chordid
+           predecessor.Address = successor.Address
+           predecessor.Chordid = successor.Chordid
+           fingertable[1] = NodeInfo{chordid,selfaddr}
+           fmt.Println("createChordRing: successor =", successor.Address)
+}
+
+func joinChordRing(naddr string) error {
+           predecessor.Chordid = -1
+           service := naddr
+           client, err := jsonrpc.Dial(protocol, service)
+           if err != nil {
+                log.Fatal("dialing:", err)
+           }
+           var reply NodeInfo
+           request := NodeInfo{chordid,selfaddr}
+           RpcCall := client.Go("DIC3.FindSuccessor", request, &reply,nil)
+           replyCall := <-RpcCall.Done
+           if replyCall != nil {
+           }
+           successor.Address = reply.Address
+           fingertable[1] = NodeInfo{reply.Chordid,successor.Address}
+           fmt.Println("Joined chord ring")
+           fmt.Println("joinChordRing: successor =", successor.Address)
+           client.Close()
+           return nil
+
+}
+
 func findSuccessor(nid int) string {
 
-           val := fingertable[nid]
-           if val != "" {
-              return val;
+           var diff int
+           if nid > chordid {
+              diff = nid - chordid
            } else {
-             for k := 1; k< 32; k++ {
-                 nextone := (nid + k) % 32
-                 val1 := fingertable[nextone]
-                 if val1 != "" {
-                    return val1
-                 }
-             }
+              diff = nid + (32 - chordid)
            }
-           return fingertable[0]
+
+           ind := int(math.Log2(float64(diff)))
+           return fingertable[(ind+1)].Address
+
+}
+
+func nearestPredecessor(nid int) NodeInfo {
+       flen := len(fingertable)
+       for j:= flen; j > 0 ; j-- {
+           if belongsto(fingertable[j].Chordid , chordid, nid) {
+              return fingertable[j]
+           }
+       }
+       return fingertable[0]
 }
 
 func checkIfPresent(a string, list []string) bool {
